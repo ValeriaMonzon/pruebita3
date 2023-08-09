4e8471dd1c95903bd7c5c5cf4095dd90f4fd5387
ValeriaMonzon <vale.friga.monzon@gmail.com>
2023-08-09 19:08:12 -0300

    2
    reparado

---
+++ src/guia5ejer4p1/Guia5Ejer4P1.java
@@ -0,0 +1,45 @@
+/*
+ * En un nuevo proyecto, en el método main de su clase principal, se pide realizar un algoritmo
+que rellene un matriz de 4 x 4 de valores aleatorios y muestre la traspuesta de la matriz.
+ */
+package guia5ejer4p1;
+
+/**
+ *
+ * @author valeria
+ */
+public class Guia5Ejer4P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        int[][] matriz = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+               
+            for (int j = 0; j < 4; j++) {
+               
+                matriz[i][j] = (int) (Math.random() * 10);
+                System.out.print(matriz [i][j]);
+                
+            }
+           System.out.println(" ");   
+        }
+        System.out.println("------------------------");
+           for (int j = 0; j < 4; j++) {
+               
+            for (int i = 0; i < 4; i++) {
+               
+                //matriz[i][j] = (int) (Math.random() * 10);
+                System.out.print(matriz [i][j]);
+                
+            }
+           System.out.println(" ");   
+        }
+      
+    }
+                   
+}
